ceb65aecb15dd392520cbee78dca79bfd03c28bc
Marcin Wojtania <marcin.wojtania@gmail.com>
2019-10-06 23:57:27 +0200

    Style chat window and add useWindowDimension custom hooks

--- src/components/OnlineUsers/OnlineUsers.tsx
+++ src/components/OnlineUsers/OnlineUsers.tsx
@@ -1,26 +1,36 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import getOnlineUsers from '../../redux/selectors/getUsersInRoom';
 import { OnlineUsersProps, OnlineUsersStoreProps } from './types';
 import { connect } from 'react-redux';
 import { AppState } from '../../redux/store/configureStore';
-import { User } from '../../redux/store/types';
 import OnlineUsersItem from '../OnlineUsersItem/OnlineUsersItem';
+import useWindowDimension from '../../utils/Hooks/useWindowDimension/useWindowDimension';
 
 export const OnlineUsers = (props: OnlineUsersProps) => {
-  // const onClickHandle = (user: User) => {
-  //   //console.log('priv invitation have been send,,,');
-  // }
+  const { width } = useWindowDimension();
+  const [isMobile, setIsMobile] = useState(width < 450);
+  const [showMenu, setShowMenu] = useState(false);
+
+  useEffect(() => {
+    setIsMobile(width < 450);
+  }, [width]);
+
+  const onClickHandle = () => {
+    if (isMobile) {
+      setShowMenu(!showMenu);
+    }
+  };
+
   return (
-    <div className="chat-window__online-users">
-      <span>Online users:</span>
-      <ul>
+    <div className={`chat-window__online-users ${isMobile ? (showMenu ? 'show' : 'hide') : ''}`} >
+      <span onClick={onClickHandle}>Online users:</span>
+      <ul className={`${isMobile ? (showMenu ? 'show' : 'hide') : ''}`}>
         {
           props.users.length === 0 ? (
             <span>No online users</span>
           ) : (
             props.users.map((user, index) => (
               <OnlineUsersItem key={index} user={user}/>
-              // <li key={index}>{user.nickname}<button>Priv</button></li>
             ))
           )
         }
